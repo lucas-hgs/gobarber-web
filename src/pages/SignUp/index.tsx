588519c5a4d1682d7ee1a4a79ff73340ae6154c0
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2020-09-23 17:26:14 -0300

    Added Unform to manipulate the Inputs of the App

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,6 +10,10 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
@@ -16,7 +21,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="Logo GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Sign up</h1>
 
           <Input name="name" icon={FiUser} placeholder="Name" />
@@ -31,7 +36,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Sign up</Button>
-        </form>
+        </Form>
 
         <a href="create">
           <FiArrowLeft />
