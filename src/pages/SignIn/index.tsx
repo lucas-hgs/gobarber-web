5ce7115a6951063bd73e9c33a8b688a00c731867
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2020-09-23 21:16:37 -0300

    Added validation to the Log In page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import getValidationErrors from '../../utils/getValidationErrors';
+
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email is required!')
+          .email('Please enter with a valid email address!'),
+        password: Yup.string().required('Password is required!'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="Logo GoBarber" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Sign in</h1>
           <Input name="email" icon={FiMail} placeholder="E-mail" />
 
@@ -28,7 +56,7 @@ const SignIn: React.FC = () => {
           <Button type="submit">Sign in</Button>
 
           <a href="forgot">Forgot password?</a>
-        </form>
+        </Form>
 
         <a href="create">
           <FiLogIn />
