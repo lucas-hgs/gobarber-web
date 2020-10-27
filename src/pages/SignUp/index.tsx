de0f3f8a90ece5c9b5d3b59833aa2256c3851970
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2020-10-26 21:24:13 -0300

    Created routes to the application

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,6 +3,7 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -11,7 +12,7 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
@@ -43,29 +44,31 @@ const SignUp: React.FC = () => {
       <Background />
 
       <Content>
-        <img src={logoImg} alt="Logo GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="Logo GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Sign up</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Sign up</h1>
 
-          <Input name="name" icon={FiUser} placeholder="Name" />
+            <Input name="name" icon={FiUser} placeholder="Name" />
 
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Password"
-          />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Password"
+            />
 
-          <Button type="submit">Sign up</Button>
-        </Form>
+            <Button type="submit">Sign up</Button>
+          </Form>
 
-        <a href="create">
-          <FiArrowLeft />
-          Go back to Sign in
-        </a>
+          <Link to="/">
+            <FiArrowLeft />
+            Go back to Sign in
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   );
